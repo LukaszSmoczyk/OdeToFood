77871b6458ce559a73ac7371dd7b6ffd526dab94
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2020-09-27 16:53:44 +0200

    Add project files.

---
+++ OdeToFood.Core/CusineType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OdeToFood.Core
+{
+    public enum CusineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CusineType.Italian},
+                new Restaurant {Id = 2, Name = "Mahatma Gandi", Location = "Delaware", Cuisine = CusineType.Indian},
+                new Restaurant {Id = 3, Name = "Taco Bell", Location = "New York", Cuisine = CusineType.Mexican },
+                new Restaurant {Id = 4, Name = "Piregie's", Location = "Illnois", Cuisine = CusineType.None},
+            };
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            var result = from r in restaurants
+                         where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                         orderby r.Name
+                         select r;
+            return result;
+        }
+    }
+}
