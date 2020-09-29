8c80ea214b62dc22ba5e4bd749571424dbfabc37
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2020-09-30 00:11:05 +0200

    Adding SQL Database

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -11,60 +11,10 @@ namespace OdeToFood.Data
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
         int Commit();
     }
 
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
 
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
-                new Restaurant {Id = 2, Name = "Mahatma Gandi", Location = "Delaware", Cuisine = CuisineType.Indian},
-                new Restaurant {Id = 3, Name = "Taco Bell", Location = "New York", Cuisine = CuisineType.Mexican },
-                new Restaurant {Id = 4, Name = "Piregie's", Location = "Illnois", Cuisine = CuisineType.None},
-            };
-        }
 
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            return newRestaurant;
-        }
-
-        public int Commit()
-        {
-            return 0;
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            var result = from r in restaurants
-                         where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                         orderby r.Name
-                         select r;
-            return result;
-        }
-
-        public Restaurant Update(Restaurant updatedRestaurant)
-        {
-            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
-            if(restaurant != null)
-            {
-                restaurant.Name = updatedRestaurant.Name;
-                restaurant.Location = updatedRestaurant.Location;
-                restaurant.Cuisine = updatedRestaurant.Cuisine;
-            }
-            return restaurant;
-        }
-    }
 }
